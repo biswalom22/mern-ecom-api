0fb692113a12db9cb47791755f13571b903c2e0a
Date The Ramp <omprakashb@datetheramp.com>
2025-04-02 02:04:32 +0530

    feat: Added enum for OrderStatus

---
+++ src/enums/enums.ts
@@ -0,0 +1,12 @@
+export enum OrderStatus {
+    PENDING = 'pending',
+    SHIPPED = 'shipped',
+    DELIVERED = 'delivered',
+  }
+
+  export enum ReturnStatus {
+    PENDING = 'pending',
+    APPROVED = 'approved',
+    REJECTED = 'rejected',
+    COMPLETED = 'completed',
+  }
